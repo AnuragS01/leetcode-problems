c52499fe6bda1ee1c69bbd20f19a5584dbaacf87
Anurag Singh <anuragsatyamsingh@gmail.com>
2026-01-20 11:08:27 +0530

    Time: 25 ms (13.54%) | Memory: 58.2 MB (51.55%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
@@ -2,17 +2,17 @@
  * @param {string[]} tokens
  * @return {number}
  */
-var evalRPN = function(tokens) {
+var evalRPN = function (tokens) {
     let stack = [];
 
-    for(let i=0;i<tokens.length;i++){
-        if(["+","-","*","/"].includes(tokens[i])){
+    for (let i = 0; i < tokens.length; i++) {
+        if (["/", "+", "-", "*"].includes(tokens[i])) {
             let prev1 = stack.pop();
             let prev2 = stack.pop();
 
-            let val = eval(`${prev2} ${tokens[i]} ${prev1}`);
-            stack.push(Math.trunc(val));
-        }else {
+            stack.push(Math.trunc(eval(`${prev2} ${tokens[i]} ${prev1}`)));
+        }
+        else {
             stack.push(tokens[i]);
         }
     }
