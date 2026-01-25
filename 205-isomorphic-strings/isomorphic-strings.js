465579a43c1212806c7717d1743303ce63b50b32
Anurag Singh <anuragsatyamsingh@gmail.com>
2026-01-25 23:19:19 +0530

    Time: 7 ms (47.59%) | Memory: 55.2 MB (60.09%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+    if (s.length !== t.length) return false;
+    let sMap = new Map();
+    let tMap = new Map();
+
+    for (let i = 0; i < s.length; i++) {
+        if (!sMap.has(s[i])) sMap.set(s[i], t[i]);
+        if (!tMap.has(t[i])) tMap.set(t[i], s[i]);
+
+        if (sMap.get(s[i]) !== t[i] || tMap.get(t[i]) !== s[i]) return false;
+    }
+
+    return true;
+};
