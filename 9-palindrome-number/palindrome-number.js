4d035008de236943d55d90d0bd952648bbc1f8bf
Anurag Singh <anuragsatyamsingh@gmail.com>
2026-02-07 17:59:48 +0530

    Time: 6 ms (65.97%) | Memory: 64.4 MB (48.04%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+    if (x < 0) return false;
+    x = String(x);
+    let left = 0, right = x.length-1;
+
+    while(left < right){
+        if(x[left] !== x[right]) return false;
+        left++;
+        right--;
+    }
+
+    return true;
+};
